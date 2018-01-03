8a29dae676e16f9a8689eb3e0fb29b0846181927
Bill Jaggers <wjaggers@aavsb.local>
2018-01-03 14:31:34 -0600

    add rest of files

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2eaf1c6f-4102-4726-9e3d-134865ad52c8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTesting.Tests
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        private const string CHROME_DRIVER_PATH = @"C:\SeleniumDriver";
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void BillsSweetTest()
+        {
+            IWebDriver driver = new ChromeDriver(CHROME_DRIVER_PATH);
+            driver.Navigate().GoToUrl("http://localhost:54381/");
+
+            IWebElement checkBox = driver.FindElement(By.Id("cbBillCheckMe"));
+            checkBox.Click();
+
+            Assert.That(checkBox.Selected == true);
+        }
+    }
+}
